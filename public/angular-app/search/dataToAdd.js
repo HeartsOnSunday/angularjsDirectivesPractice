e992038a29f89d4f1ef627211a4447bebf0c8cae
Josh Bristol <josh.bristol2@gmail.com>
2018-03-07 00:49:52 +0000

    blank project template

---
+++ public/angular-app/search/dataToAdd.js
@@ -0,0 +1,26 @@
+
+	
+	    [{
+			firstName: 'Josh',
+			lastName: 'Bristol'
+		},
+		{
+			firstName: 'John',
+			lastName: 'Smith'
+		},
+		{
+			firstName: 'Nick',
+			lastName: 'Johnson'
+		},
+		{
+			firstName: 'Sarah',
+			lastName: 'Rogers'
+		},
+		{
+			firstName: 'Rachel',
+			lastName: 'Roberts'
+		}
+	];
+
+
+['Alex', 'Josh', 'Matt', 'Alex']
